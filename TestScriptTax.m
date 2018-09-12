bec934666d1ccdee146a176757e78d977caf447d
Hamza <hah2143@columbia.edu>
2018-09-12 16:22:57 -0400

    (i) Edited TestScriptTax.m so that it is up to date with TestScriptOil.m and made sure it ran without problems (ii) Edited data files in Tax folder so that they run without issues

--- TestScriptTax.m
+++ TestScriptTax.m
@@ -5,68 +5,78 @@
 %         Running Matlab R2018a.
 %         This script runs in about 11 seconds. 
 
-clear
+clear;
 
-tic 
+tic
 
 direct = pwd;
 
+disp('(We would like to thank Qifan Han and Jianing Zhai for excellent research assistance)')
+
 %% 1) Set number of VAR Lags, Newey West lags and confidence level.
 
-fprintf('This script is an example on how to use the function SVARIV_General to report confidence intervals for  IRFs \n');
+fprintf('\nThis script uses the function SVARIV to report confidence intervals for  IRFs \n');
 
-fprintf('estimated using the SVAR-IV approach described in MSW(18).\n');
+fprintf('as described in Montiel Olea, Stock, and Watson(18).\n');
 
-disp('-');
+disp('--');
 
-disp('Section 1 describes sets values for VAR Lags, Newey West lags and confidence level, VAR variable names, time unit,');
+fprintf('Section 1 sets values for: \n')
 
-disp('variable used for normalization, scale of the shock and the number of horizons for the IRFs. It also allows the user');
+fprintf('-VAR Lags \n')
+fprintf('-Newey West lags (if desired) \n')
+fprintf('-Confidence level \n')
+fprintf('-VAR variable names \n')
+fprintf('-Time unit \n')
+fprintf('-Variable used for normalization \n')
+fprintf('-Scale of the shock \n')
+fprintf('-Number of horizons for the IRFs. \n')
 
-disp('to select specific variables of interest for the IRF plots and cumulative IRF plots');
+disp('This section also allows the user to select specific variables of interest for the IRF plots');
 
-application = 'Fiscal';  % Name of this empirical application, used for creating and accessing folders
+application = 'Tax';  % Name of this empirical application. This name will be used for creating and accessing folders
 
-p           = 2;      % Number of lags in the VAR model
+p           = 2;     %Number of lags in the VAR model
  
-confidence  = .68;    % Confidence Level for the standard and weak-IV robust confidence set
+confidence  = .95;    %Confidence Level for the standard and weak-IV robust confidence set
 
-                      % Define the variables in the SVAR
+% Define the variables in the SVAR
 columnnames = [{'Log(1/1-AMTR)'}, ...
                {'Log Income'}, ...
                {'Log Real GDP'},...
                {'Unemployment Rate'},...
                {'Inflation'}, {'FFR'}, {'Log GOV'}, {'Log RSTPrices'}, {'DLOG RDEBT'}];
 
-time        = 'Year'; % Time unit for the dataset (e.g. year, month, etc).
+time        = 'Year';  % Time unit for the dataset (e.g. year, month, etc).
 
-NWlags      = 8;      % Newey-West lags(if it is neccessary to account for time series autocorrelation)
-                      % (set it to 0 to compute heteroskedasticity robust std errors)
+NWlags      = 8;  % Newey-West lags(if it is neccessary to account for time series autocorrelation)
+                  % (set it to 0 to compute heteroskedasticity robust std errors)
 
-norm        = 1;      % Variable used for normalization
+norm        = 1; % Variable used for normalization
 
-scale       = -1;     % Scale of the shock
+scale       = -1; % Scale of the shock
 
-horizons    = 5;      % Number of horizons for the Impulse Response Functions(IRFs)
-                      %(does not include the impact or horizon 0)
+horizons    = 5; %Number of horizons for the Impulse Response Functions(IRFs)
+                 %(does not include the impact or horizon 0)
                  
-IRFselect   = [];
+IRFselect   = [1,2,3,4];
 % By default, the program generates a single figure with the IRFs for ALL variables
 % in the VAR. However, IRFselect allows the user to generate an indepedent
 % figure displaying only some specific variables of interest. 
-% The program also saves the IRFs in IRFselect as separate .eps files 
+% For example, IRFselect   = [1,2] generates individual IRF figures for
+% variables 1 and 2 and saves them as separate .eps files 
 
 % Make sure to match the indices above and to the variables in your
-% dataset. E.g. the above vector will select the variables "AMTR,
-% Log Real GDP, Inflation and DLOG RDEBT."
+% dataset. E.g. the IRFselect   = [1,2] will select the variables 
+% "Index of real economic activity" and the "Real Price of Oil".
 
-cumselect = []; 
+cumselect = [5,6]; 
 %cumselect allows the user to generate cumulative IRF plots displaying only 
 %some specific variables of interest. 
 
 % Make sure to match the indices above to the variables in your
-% dataset. E.g. the above vector will select the variables "AMTR,
-% Log Real GDP, Inflation and DLOG RDEBT."
+% dataset.
+
 
 %% 2) Load data (saved in structure "data")
 %  These are the variables that were defined in line 14 above. 
@@ -78,41 +88,37 @@ disp('-')
 
 disp('Section 2 in this script loads the data necessary for the SVAR-IV analysis.')
 
-cd(strcat(direct,'/Data/',application));
+cd(strcat(pwd,'/Data/',application));
  
-    years                               = xlsread('Time',... 
-                                          'A2:A60');
+    years                               = xlsread('Time');
                                         % time variable. Here as a 59x1
                                         % vector
  
-    ydata                               = xlsread('Data',...
-                                          'A2:I60');  
+    ydata                               = xlsread('Data');
+                                         
                                         % all of the endogenous variables
                                         % used in the VAR
                                       
-    z                                   = xlsread('ExtIV',...
-                                          'A2:A60');
+    z                                   = xlsread('ExtIV');
                                         %External IV, not included in the VAR. 59x1 vector.
 
 dataset_name = 'ALL(PS2003)'; % The name of the dataset used for generating the figures (used in the output label)                                      
                                         
-cd ..
-
-cd ..
+cd(direct);
  
-%% 3) Test
+%% 3) Run the SVARIV function
 
-disp('-')
+disp('--')
 
-disp('Section 3 in this script calls the SVARIV_General function')
+disp('Section 3 in this script calls the SVARIV function')
 
-savdir = strcat(direct,'/Output/', application);  %selected directory where the output files will be saved
+savdir = strcat(direct,'/Output/',application);  %selected directory where the output files will be saved
 
 addpath(strcat(direct,'/functions/MasterFunction')); 
 
 addpath(strcat(direct,'/functions/Inference'));
 
-[Plugin, InferenceMSW, Chol, RForm, figureorder] = SVARIV_General(p, confidence, ydata, z, NWlags, norm, scale, horizons, savdir, columnnames, IRFselect, cumselect, time, dataset_name);
+[Plugin, InferenceMSW, Chol, RForm, figureorder, ARxlim, ARylim] = SVARIV(p, confidence, ydata, z, NWlags, norm, scale, horizons, savdir, columnnames, IRFselect, cumselect, time, dataset_name);
  
 % A more in depth description of the function can be found within the
 % function file. For clarity purposes, we briefly describe the function
@@ -138,13 +144,61 @@ addpath(strcat(direct,'/functions/Inference'));
 %       InferenceMSW: Structure containing the MSW weak-iv robust confidence interval
 %       Chol: Cholesky IRFs
 
+%% 4) "Standard" bootstrap-type inference based on samples from the asy dist.
+
+disp('Section 4 calls the Gasydistboots function to provide inference for SVAR-IV based on samples from the asy. dist.')
+
+seed            = load(strcat(direct,'/seed/seedMay12.mat')); 
+    
+seed            = seed.seed;
+
+n = RForm.n; 
+
+T  = size(ydata, 1);            % Number of observations/time periods.
+
+NB = 1000;                      % Number of bootstrap replications 
+
+SVARinp.Z = z;
+
+SVARinp.ydata = ydata;
+
+SVARinp.n = n;
 
 
+addpath('functions/Inference');
 
-%% 4) Bootstrap Plots
+[~,InferenceMSW.bootsIRFs] = ...
+                  Gasydistboots(seed, NB, n, p, norm, scale, horizons, confidence, T,...
+                  @IRFSVARIV, SVARinp, NWlags, RForm.AL, RForm.Sigma, RForm.Gamma, RForm.V, RForm.WHatall);
 
-disp('Section 4 in this script calls the Bootstrap_Plots function.')
+%% 5) Bootstrap Plots
 
-[caux,InferenceMSW,NB,seed,SVARinp,T] = Bootstrap_Plots(ydata, z, p, norm, scale, horizons, confidence, NWlags, RForm, figureorder, Plugin, InferenceMSW, time, columnnames, savdir, direct, dataset_name, IRFselect, cumselect);
+disp('Section 5 calls the Bootstrap_Plots function.')
 
-toc
+addpath(strcat(direct,'/functions/figuresfun'));
+
+[caux,InferenceMSW, figureorder] = Bootstrap_Plots(n, p, horizons, confidence, RForm, SVARinp, figureorder, Plugin, InferenceMSW, time, columnnames, savdir, direct, dataset_name, IRFselect, cumselect);
+
+
+%% 6) AR confidence set using bootstrap implementation
+
+disp('Section 6 in this script calls the GasydistbootsAR function to do the bootstrap implementation of the Anderson-Rubin confidence set')
+
+cd(strcat(direct,'/functions/Inference'));
+
+multiplier = 1.5;     %Scalar that GasydistbootsAR will use to create the "grid" of null hypothesis for IRFs
+                     %IRFhat +- multiplier*standard errors
+
+grid_size = 50;      % Number of mull hypotheses (lambdas) in the grid, for each variable and for each horizon.
+
+[reject, bootsIRFs, gridpointupperMSW, gridpointlowerMSW, null_grid] = GasydistbootsAR(ydata, T, seed, RForm.n, NB, p, norm, scale, horizons, confidence, SVARinp, NWlags, RForm.AL, RForm.Sigma, RForm.Gamma, RForm.V, RForm.WHatall, Plugin, multiplier, grid_size, ARylim);
+
+%% 7) AR Bootstrap plots
+
+disp('Section 7 calls the BootstrapAR_Plots function.')
+
+addpath(strcat(direct,'/functions/figuresfun'));
+
+BootstrapAR_Plots(n, p, horizons, confidence, RForm, SVARinp, figureorder, Plugin, InferenceMSW, time, columnnames, savdir, direct, dataset_name, IRFselect, cumselect, reject, null_grid, norm);
+
+toc; 
