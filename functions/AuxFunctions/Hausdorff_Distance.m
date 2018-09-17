0f1766e4267ecccce33bb074ce7f9b02a12b36f6
Hamza <hah2143@columbia.edu>
2018-09-17 17:37:50 -0400

    Made edits to the Hausdorff_Distance function so that it goes through all of the possible cases

--- functions/AuxFunctions/Hausdorff_Distance.m
+++ functions/AuxFunctions/Hausdorff_Distance.m
@@ -1,6 +1,5 @@
-function [hd,hdistance,hdistance2] = Hausdorff_Distance(n, horizons, MSWlbound, MSWubound,MSWlboundcum, MSWuboundcum, Dmethodlbound, Dmethodubound, Dmethodlboundcum, Dmethoduboundcum)
-%-Reports the Hausdorff distance between MSW & Delta-Method confidence
-%intervals
+function [hd,hdistanceMSW,hdistanceDMethod] = Hausdorff_Distance(n, horizons, MSWlbound, MSWubound,MSWlboundcum, MSWuboundcum, Dmethodlbound, Dmethodubound, Dmethodlboundcum, Dmethoduboundcum)
+%-Reports the Hausdorff distance between MSW & Delta-Method confidence intervals
 %    -Syntax:
 %       [hd,hdistance,hdistance2] = Hausdorff_Distance(n, horizons, MSWlbound, MSWubound,MSWlboundcum, MSWuboundcum, Dmethodlbound, Dmethodubound, Dmethodlboundcum, Dmethoduboundcum)
 %    -Inputs:
@@ -17,195 +16,215 @@ function [hd,hdistance,hdistance2] = Hausdorff_Distance(n, horizons, MSWlbound, 
 %       Dmethoduboundcum:    upper bounds of delta method cumulative confidence interval                (n x horizons+1)
 %
 %   -Outputs:
-%       hd: Hausdorff distances computed for each variable (this was to
-%           check the way Pepe suggested doing it)
-%       hdistance: Grid of Hausdorff distances for each variable and each
-%           horizon (likely not the correct way, keeping just in case) 
-%       hdistance2: Hausdorff distances computed for each variable (I think
-%           the way Pepe suggested doing it)
+%       hd: Hausdorff distances computed for each variable and horizon (maximum of hdistanceMSW & hdistanceDMethod)  
+%       hdistanceMSW: Distance starting from furthest point in MSW CI to closest point in DMethod CI 
+%       hdistanceDMethod: Distance starting from furthest point in DMethod CI to closest point in MSW CI
 
+%% Hausdorff Distance 
 
-%% Initial way of trying it out, creating Hausdorff distance for each
-% %variable, each horizon (THIS IS PROBABLY WRONG!!!)
+hdistanceMSW = Inf(n, horizons+1, 2); %taking x in MSW
+hdistanceDMethod = Inf(n, horizons+1, 2); %taking x in D-method
+hd = Inf(n, horizons+1, 2);
 
-hdistance = zeros(n, horizons+1, 2);
-
-hdistance(:,:,1) = max(abs(MSWlbound-Dmethodlbound), abs(MSWubound-Dmethodubound)); 
-
-hdistance(:,:,2) = max(abs(MSWlboundcum-Dmethodlboundcum), abs(MSWuboundcum-Dmethoduboundcum));
-
-%% Second way of trying it out, creating one Hausdorff distance for each
-%variable
-
-hdistance2 = zeros(n,2); 
-
-for var = 1:n 
-    
-    %Calculating max and min bounds for non-cumulative MSW & Dmethod for each variable and
-    %then taking hdistance
-    
-    MSWlbound2 = min(MSWlbound(var,:)); 
-    
-    MSWubound2 = max(MSWubound(var,:)); 
-    
-    Dmethodlbound2 = min(Dmethodlbound(var,:));
-    
-    Dmethodubound2 = max(Dmethodubound(var,:));
-    
-    hdistance2(var,1) = max(abs(MSWlbound2 - Dmethodlbound2), abs(MSWubound2 - Dmethodubound2));
-    
-    
-    %Calculating max and min bounds for cumulative MSW & Dmethod for each variable and
-    %then taking hdistance
-    MSWlboundcum2 = min(MSWlboundcum(var,:)); 
-    
-    MSWuboundcum2 = max(MSWuboundcum(var,:)); 
-    
-    Dmethodlboundcum2 = min(Dmethodlboundcum(var,:));
-    
-    Dmethoduboundcum2 = max(Dmethoduboundcum(var,:));
-    
-    hdistance2(var,2) = max(abs(MSWlboundcum2 - Dmethodlboundcum2), abs(MSWuboundcum2 - Dmethoduboundcum2)); 
-    
-end
-
-
-%% 3rd way of trying it out 
 %Non-cumulative 
 
-MSWbounds = horzcat(MSWlbound, MSWubound); 
+%For purposes of calculating distance from MSW to DMethod
+a = MSWlbound;
+b = MSWubound;
+c = Dmethodlbound; 
+d = Dmethodubound;
 
-Dmethodbounds = horzcat(Dmethodlbound, Dmethodubound); 
+%For purposes of calculating distance from DMethod to MSW
+e = Dmethodlbound;
+f = Dmethodubound;
+g = MSWlbound; 
+h = MSWubound;
 
-%Distances from MSWbounds to Dmethodbounds
-d = zeros(size(MSWbounds,2),size(Dmethodbounds,2),n); 
+%Cumulative
 
-shortest = Inf(size(MSWbounds,2),n);
+%For purposes of calculating distance from MSW to DMethod
+acum = MSWlboundcum;
+bcum = MSWuboundcum;
+ccum = Dmethodlboundcum; 
+dcum = Dmethoduboundcum;
 
-h1 = zeros(n,1);
+%For purposes of calculating distance from DMethod to MSW
+ecum = Dmethodlboundcum;
+fcum = Dmethoduboundcum;
+gcum = MSWlboundcum; 
+hcum = MSWuboundcum;
 
-for var = 1:n 
+for var = 1:n
     
-    for i = 1:size(MSWbounds,2) 
+    for hori = 1:horizons+1
+        %Starting in MSW CI, finding distance to closest point in DMethod CI
         
-        for j = 1:size(Dmethodbounds,2)
-            
-            d(i,j,var) = abs(MSWbounds(var,i) - Dmethodbounds(var,j));
-            
-        end   
-        
-        if min(d(i,:,var)) < shortest(i,var)
+        %No intersections
+        if b(var,hori) < c(var,hori) 
 
-            shortest(i,var) = min(d(i,:,var));
+            hdistanceMSW(var,hori,1) = abs(c(var,hori) - a(var,hori)); 
 
-        else           
         end
- 
-    end
 
-    h1(var,1) = max(shortest(:,var)); 
+        if a(var,hori) > d(var,hori) 
+
+            hdistanceMSW(var,hori,1) = abs(b(var,hori) - d(var,hori)); 
+
+        end
+        
+        %Intersections
+        if b(var,hori) >= c(var,hori) && b(var,hori) <= d(var,hori) && a(var,hori) < c(var,hori)   
+
+            hdistanceMSW(var,hori,1) = abs(a(var,hori) - c(var,hori)); 
+
+        end 
+
+        if a(var,hori) >= c(var,hori) && a(var,hori) <= d(var,hori) && b(var,hori) > d(var,hori) 
+
+            hdistanceMSW(var,hori,1) = abs(b(var,hori) - d(var,hori));
+
+        end 
+
+        if a(var,hori) >= c(var,hori) && a(var,hori) <= d(var,hori) && b(var,hori) >= c(var,hori) && b(var,hori) <= d(var,hori) 
+
+            hdistanceMSW(var,hori,1) = 0;
+
+        end
+
+        if a(var,hori) <= c(var,hori) && b(var,hori) >= d(var,hori) 
+
+            hdistanceMSW(var,hori,1) = max(abs(c(var,hori)-a(var,hori)),abs(d(var,hori)-b(var,hori))); 
+
+        end
+        
+        %Starting in DMethod CI, finding distance to closest point in MSW CI
+
+        if f(var,hori) < g(var,hori) %Case where [a,b] (MSW CI) < [c,d](D-Method CI) (no intersection)
+
+            hdistanceDMethod(var,hori,1) = abs(g - e); 
+
+        end
+
+        if e(var,hori) > h(var,hori) %Case where [a,b] > [c,d] (no intersection)
+
+            hdistanceDMethod(var,hori,1) = abs(f(var,hori) - h(var,hori)); 
+
+        end
+
+        if f(var,hori) >= g(var,hori) && f(var,hori) <= h(var,hori) && e(var,hori) < g(var,hori)  %Case where c <= b <= d 
+
+            hdistanceDMethod(var,hori,1) = abs(e(var,hori) - g(var,hori)); 
+
+        end 
+
+        if e(var,hori) >= g(var,hori) && e(var,hori) <= h(var,hori) && f(var,hori) > h(var,hori) %Case where c <= a <= d 
+
+            hdistanceDMethod(var,hori,1) = abs(f(var,hori) - h(var,hori));
+
+        end 
+
+        if e(var,hori) >= g(var,hori) && e(var,hori) <= h(var,hori) && f(var,hori) >= g(var,hori) && f(var,hori) <= h(var,hori) 
+
+            hdistanceDMethod(var,hori,1) = 0;
+
+        end
+
+        if e(var,hori) <= g(var,hori) && f(var,hori) >= h(var,hori) 
+
+            hdistanceDMethod(var,hori,1) = max(abs(g(var,hori)-e(var,hori)),abs(h(var,hori)-f(var,hori))); 
+
+        end
+
+        %Cumulative 
+        
+        %Starting in MSW CI, finding distance to closest point in DMethod CI
+        
+        %No intersections
+
+        if bcum(var,hori) < ccum(var,hori)
+
+            hdistanceMSW(var,hori,2) = abs(ccum(var,hori) - acum(var,hori)); 
+
+        end
+
+        if acum(var,hori) > dcum(var,hori) 
+
+            hdistanceMSW(var,hori,2) = abs(bcum(var,hori) - dcum(var,hori)); 
+
+        end
+        
+        %Intersections
+
+        if bcum(var,hori) >= ccum(var,hori) && bcum(var,hori) <= dcum(var,hori) && acum(var,hori) < ccum(var,hori)   
+
+            hdistanceMSW(var,hori,2) = abs(acum(var,hori) - ccum(var,hori)); 
+
+        end 
+
+        if acum(var,hori) >= ccum(var,hori) && acum(var,hori) <= dcum(var,hori) && bcum(var,hori) > dcum(var,hori)  
+
+            hdistanceMSW(var,hori,2) = abs(bcum(var,hori) - dcum(var,hori));
+
+        end 
+
+        if acum(var,hori) >= ccum(var,hori) && acum(var,hori) <= dcum(var,hori) && bcum(var,hori) >= ccum(var,hori) && bcum(var,hori) <= dcum(var,hori) 
+
+            hdistanceMSW(var,hori,2) = 0;
+
+        end
+
+        if acum(var,hori) <= ccum(var,hori) && bcum(var,hori) >= dcum(var,hori) 
+
+            hdistanceMSW(var,hori,2) = max(abs(ccum(var,hori)-acum(var,hori)),abs(dcum(var,hori)-bcum(var,hori))); 
+
+        end 
+
+        %Starting in DMethod CI, finding distance to closest point in MSW CI 
+        
+        %No intersections
+        
+        if fcum(var,hori) < gcum(var,hori) 
+
+            hdistanceDMethod(var,hori,2) = abs(gcum(var,hori) - ecum(var,hori)); 
+
+        end
+
+        if ecum(var,hori) > hcum(var,hori) 
+
+            hdistanceDMethod(var,hori,2) = abs(fcum(var,hori) - hcum(var,hori)); 
+
+        end
+        
+        %Intersections
+
+        if fcum(var,hori) >= gcum(var,hori) && fcum(var,hori) <= hcum(var,hori) && ecum(var,hori) < gcum(var,hori)   
+
+            hdistanceDMethod(var,hori,2) = abs(ecum(var,hori) - gcum(var,hori)); 
+
+        end 
+
+        if ecum(var,hori) >= gcum(var,hori) && ecum(var,hori) <= hcum(var,hori) && fcum(var,hori) > hcum(var,hori) 
+
+            hdistanceDMethod(var,hori,2) = abs(fcum(var,hori) - hcum(var,hori));
+
+        end 
+
+        if ecum(var,hori) >= gcum(var,hori) && ecum(var,hori) <= hcum(var,hori) && fcum(var,hori) >= gcum(var,hori) && fcum(var,hori) <= hcum(var,hori) 
+
+            hdistanceDMethod(var,hori,2) = 0;
+
+        end
+
+        if ecum(var,hori) <= gcum(var,hori) && fcum(var,hori) >= hcum(var,hori) 
+
+            hdistanceDMethod(var,hori,2) = max(abs(gcum(var,hori)-ecum(var,hori)),abs(hcum(var,hori)-fcum(var,hori))); 
+
+        end
+        
+    end
     
 end
 
-%Distances from Dmethodbounds to MSWbounds
-d = zeros(size(MSWbounds,2),size(Dmethodbounds,2),n); 
-
-shortest = Inf(size(MSWbounds,2),n);
-
-h2 = zeros(n,1);
-
-for var = 1:n 
-    
-    for i = 1:size(Dmethodbounds,2) 
-        
-        for j = 1:size(MSWbounds,2)
-            
-            d(i,j,var) = abs(Dmethodbounds(var,i) - MSWbounds(var,j));
-            
-        end   
-        
-        if min(d(i,:,var)) < shortest(i,var)
-
-            shortest(i,var) = min(d(i,:,var));
-
-        else           
-        end
- 
-    end
-
-    h2(var,1) = max(shortest(:,var)); 
-    
-end
-
-%Hausdorff distances BETWEEN bounds (non-cumulative)
-hd(:,1) = bsxfun(@max,h1,h2);    
-
-%Cumulative 
-
-MSWboundscum = horzcat(MSWlboundcum, MSWuboundcum); 
-
-Dmethodboundscum = horzcat(Dmethodlboundcum, Dmethoduboundcum); 
-
-d = zeros(size(MSWboundscum,2),size(Dmethodboundscum,2),n); 
-
-shortest = Inf(size(MSWboundscum,2),n);
-
-h3 = zeros(n,1);
-
-for var = 1:n 
-    
-    for i = 1:size(MSWboundscum,2) 
-        
-        for j = 1:size(Dmethodboundscum,2)
-            
-            d(i,j,var) = abs(MSWboundscum(var,i) - Dmethodboundscum(var,j));
-            
-        end   
-        
-        if min(d(i,:,var)) < shortest(i,var)
-
-            shortest(i,var) = min(d(i,:,var));
-
-        else           
-        end
- 
-    end
-
-    h3(var,1) = max(shortest(:,var)); 
-    
-end
-
-d = zeros(size(MSWboundscum,2),size(Dmethodboundscum,2),n); 
-
-shortest = Inf(size(MSWboundscum,2),n);
-
-h4 = zeros(n,1);
-
-for var = 1:n 
-    
-    for i = 1:size(Dmethodboundscum,2) 
-        
-        for j = 1:size(MSWboundscum,2)
-            
-            d(i,j,var) = abs(Dmethodboundscum(var,i) - MSWboundscum(var,j));
-            
-        end   
-        
-        if min(d(i,:,var)) < shortest(i,var)
-
-            shortest(i,var) = min(d(i,:,var));
-
-        else           
-        end
- 
-    end
-
-    h4(var,1) = max(shortest(:,var)); 
-    
-end
-
-%Hausdorff distances BETWEEN bounds (cumulative)
-hd(:,2) = bsxfun(@max,h3,h4);    
-
+hd(:,:,:) = max(hdistanceMSW,hdistanceDMethod);
     
 end 
